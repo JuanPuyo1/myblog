d15817b9135f42053625e5ac5a24f81362f3217a
Esteban Puyo <jpuyoc@ucentral.edu.co>
2024-11-26 19:10:31 -0800

    delete de hardcoded texts and replacing them for the post info

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
         "author": "Maximilian",
-        "date": date(2021, 7, 21),
+        "date": datetime.datetime(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -28,7 +29,7 @@ posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Maximilian",
-        "date": date(2022, 3, 10),
+        "date": datetime.datetime(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -49,7 +50,7 @@ posts = [
         "slug": "into-the-woods",
         "image": "woods.jpg",
         "author": "Maximilian",
-        "date": date(2020, 8, 5),
+        "date": datetime.datetime(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -68,8 +69,16 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    #I dont want to change the original list, thats why we use the sorted function
+    sorted_posts = sorted(all_posts,key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html",{
+        "posts": latest_posts
+    })
 
 
 def posts(request):
